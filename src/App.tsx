350d9123a360232ff5c90ccc0de9c6450ae460e1
KrohnicDev <krohn.joonas@gmail.com>
2022-07-08 13:59:15 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import { useQuery } from 'react-query'
+import './App.css'
 
-function App() {
+const WORD_LENGTH = 5
+const MAX_GUESSES = 6
+
+export default function App(): JSX.Element {
+  const [guesses, setGuesses] = useState<string[]>(
+    Array.from(Array(MAX_GUESSES))
+  )
+
+  useQuery('words', fetchWords, { initialData: [] }).data ?? []
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='game'>
+      <h1>Wordle Clone</h1>
+      {guesses.map((word) => (
+        <WordRow key={word} word={word} />
+      ))}
     </div>
-  );
+  )
 }
 
-export default App;
+interface WordRowProps {
+  word: string | undefined
+}
+
+function WordRow({ word }: WordRowProps): JSX.Element {
+  const iterable = word !== undefined ? [...word] : range(WORD_LENGTH)
+  return (
+    <div className='row'>
+      {iterable.map((m) => (
+        <CharacterCell key={m} char={typeof m === 'string' ? m : undefined} />
+      ))}
+    </div>
+  )
+}
+
+interface CharacterProps {
+  char: string | undefined
+}
+
+function CharacterCell({ char }: CharacterProps): JSX.Element {
+  return <div className='cell'>{char}</div>
+}
+
+async function fetchWords(): Promise<string[]> {
+  await sleep(500)
+  const response = await fetch('all_words.json')
+  return (await response.json()) as string[]
+}
+
+async function sleep(ms: number): Promise<void> {
+  await new Promise((resolve) => setTimeout(() => resolve(''), ms))
+}
+
+function range(size: number): number[] {
+  return Array.from(Array(size).keys())
+}
