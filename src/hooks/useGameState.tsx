4b72ac8c7c6c1eeecea55d7cd99b1ab3e06aa05c
Joonas Krohn <57285307+KrohnicDev@users.noreply.github.com>
2023-04-05 15:49:25 +0300

    Refactor

--- src/hooks/useGameState.tsx
+++ src/hooks/useGameState.tsx
@@ -1,21 +1,21 @@
 import { useMemo } from 'react'
 import { MAX_GUESSES } from '../constants'
 import { GamePhase, GameState } from '../types'
+import { match } from '../utils'
 import { useGameEngine } from './useGameEngine'
 
 /** Provides access to global game state */
 export function useGameState(): GameState {
   const { guesses, solution, ...rest } = useGameEngine()
 
-  const phase: GamePhase = useMemo(() => {
-    if (guesses.includes(solution)) {
-      return 'win'
-    }
-    if (guesses.length >= MAX_GUESSES) {
-      return 'lose'
-    }
-    return 'in-progress'
-  }, [guesses, solution])
+  const phase: GamePhase = useMemo(
+    () =>
+      match(
+        [() => guesses.includes(solution), 'win'],
+        [() => guesses.length >= MAX_GUESSES, 'lose']
+      ) ?? 'in-progress',
+    [guesses, solution]
+  )
 
   return {
     guesses,
